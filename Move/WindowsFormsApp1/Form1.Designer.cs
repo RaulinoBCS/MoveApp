7ad187cb998d5099d5c579e3d87492f1d3375408
RaulinoBCS <33448898+RaulinoBCS@users.noreply.github.com>
2020-01-19 17:09:01 +0200

    0.0.1

---
+++ Move/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SystemTrayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.Menu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.BtnStart = new System.Windows.Forms.ToolStripMenuItem();
+            this.BtnStop = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.yesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.noToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.SetTimer = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.BtnExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SystemTrayIcon
+            // 
+            this.SystemTrayIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.SystemTrayIcon.BalloonTipTitle = "App status:";
+            this.SystemTrayIcon.ContextMenuStrip = this.Menu;
+            this.SystemTrayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("SystemTrayIcon.Icon")));
+            this.SystemTrayIcon.Text = "Move";
+            this.SystemTrayIcon.Visible = true;
+            // 
+            // Menu
+            // 
+            this.Menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.BtnStart,
+            this.BtnStop,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripSeparator1,
+            this.toolStripMenuItem1,
+            this.SetTimer,
+            this.toolStripSeparator2,
+            this.BtnExit});
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(213, 177);
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(212, 22);
+            this.BtnStart.Text = "Start";
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // BtnStop
+            // 
+            this.BtnStop.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.BtnStop.Name = "BtnStop";
+            this.BtnStop.Size = new System.Drawing.Size(212, 22);
+            this.BtnStop.Text = "Stop";
+            this.BtnStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yesToolStripMenuItem,
+            this.noToolStripMenuItem});
+            this.toolStripMenuItem2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(212, 22);
+            this.toolStripMenuItem2.Text = "Run with Windows";
+            // 
+            // yesToolStripMenuItem
+            // 
+            this.yesToolStripMenuItem.Name = "yesToolStripMenuItem";
+            this.yesToolStripMenuItem.Size = new System.Drawing.Size(96, 22);
+            this.yesToolStripMenuItem.Text = "Yes";
+            this.yesToolStripMenuItem.Click += new System.EventHandler(this.yesToolStripMenuItem_Click);
+            // 
+            // noToolStripMenuItem
+            // 
+            this.noToolStripMenuItem.Name = "noToolStripMenuItem";
+            this.noToolStripMenuItem.Size = new System.Drawing.Size(96, 22);
+            this.noToolStripMenuItem.Text = "No";
+            this.noToolStripMenuItem.Click += new System.EventHandler(this.noToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem});
+            this.toolStripMenuItem3.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(212, 22);
+            this.toolStripMenuItem3.Text = "Start/Stop when starts";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(209, 6);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Enabled = false;
+            this.toolStripMenuItem1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(212, 22);
+            this.toolStripMenuItem1.Text = "Timer (minutes)";
+            // 
+            // SetTimer
+            // 
+            this.SetTimer.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.SetTimer.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.SetTimer.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.SetTimer.Name = "SetTimer";
+            this.SetTimer.Size = new System.Drawing.Size(121, 25);
+            this.SetTimer.SelectedIndexChanged += new System.EventHandler(this.SetTimer_SelectedIndexChanged);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(209, 6);
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(212, 22);
+            this.BtnExit.Text = "Exit";
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 151);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Move";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Menu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon SystemTrayIcon;
+        private System.Windows.Forms.ContextMenuStrip Menu;
+        private System.Windows.Forms.ToolStripMenuItem BtnStart;
+        private System.Windows.Forms.ToolStripMenuItem BtnStop;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripComboBox SetTimer;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem BtnExit;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem yesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem noToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+    }
+}
+
---
+++ Move/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        RegistryKey reg = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+        public Form1()
+        {
+            InitializeComponent();
+
+            BtnStop.Enabled = false;
+
+            SetTimer.SelectedIndex = Properties.Settings.Default.Timer;
+
+            if (Properties.Settings.Default.StartWhenOpen == true)
+            {
+                BtnStop.Enabled = true;
+                BtnStart.Enabled = false;
+                timer1.Interval = Properties.Settings.Default.Timer * 60000;
+                timer1.Start();
+                stopToolStripMenuItem.Enabled = true;
+                startToolStripMenuItem.Enabled = false;
+                SystemTrayIcon.ShowBalloonTip(3000, "MoveApp status:", "Running", ToolTipIcon.Info);
+            }
+            else
+            {
+
+                stopToolStripMenuItem.Enabled = false;
+                startToolStripMenuItem.Enabled = true;
+            }
+
+            if (Properties.Settings.Default.StartWithWindows == true)
+            {
+                yesToolStripMenuItem.Enabled = false;
+                noToolStripMenuItem.Enabled = true;
+            }
+            else
+            {
+                yesToolStripMenuItem.Enabled = true;
+                noToolStripMenuItem.Enabled = false;
+            }
+
+        }
+        protected override CreateParams CreateParams // 
+        {
+            get
+            {
+                var cp = base.CreateParams;
+                cp.ExStyle |= 0x80;  // Turn on WS_EX_TOOLWINDOW
+                return cp;
+            }
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            BtnStop.Enabled = true;
+            BtnStart.Enabled = false;
+            timer1.Interval = Properties.Settings.Default.Timer * 60000;
+            timer1.Start();
+            SystemTrayIcon.ShowBalloonTip(3000, "MoveApp status:", "Running", ToolTipIcon.Info);
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            BtnStop.Enabled = false;
+            BtnStart.Enabled = true;
+            timer1.Stop();
+            SystemTrayIcon.ShowBalloonTip(3000, "MoveApp status:", "Stopped", ToolTipIcon.Info);
+        }
+
+        private void SetTimer_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Timer = (int)SetTimer.SelectedIndex;
+            Properties.Settings.Default.Save();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            SystemTrayIcon.Dispose();
+            Application.Exit();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Point originalPosition = Cursor.Position;
+            Point newPosition = new Point(originalPosition.X + 5, originalPosition.Y);
+            Cursor.Position = newPosition;
+            Cursor.Position = originalPosition;
+        }
+
+        private void yesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            reg.SetValue("Move", Application.ExecutablePath.ToString());
+            yesToolStripMenuItem.Enabled = false;
+            noToolStripMenuItem.Enabled = true;
+            Properties.Settings.Default.StartWithWindows = true;
+            Properties.Settings.Default.Save();
+        }
+
+        private void noToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            reg.DeleteValue("Move", false);
+            yesToolStripMenuItem.Enabled = true;
+            noToolStripMenuItem.Enabled = false;
+            Properties.Settings.Default.StartWithWindows = false;
+            Properties.Settings.Default.Save();
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.StartWhenOpen = true;
+            Properties.Settings.Default.Save();
+            stopToolStripMenuItem.Enabled = true;
+            startToolStripMenuItem.Enabled = false;
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.StartWhenOpen = false;
+            Properties.Settings.Default.Save();
+            stopToolStripMenuItem.Enabled = false;
+            startToolStripMenuItem.Enabled = true;
+        }
+    }
+}
